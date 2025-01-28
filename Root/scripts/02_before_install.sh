38156b44cdd59cd92c8afe5d702e444b9cc8b0d8
Maninder1220 <maninderjitsinghjaura@gmail.com>
2025-01-29 05:04:15 +0530

    Cnge in buildspecyml n appspecyml - ii

--- Root/scripts/02_before_install.sh
+++ Root/scripts/02_before_install.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
 set -e
 
-echo "Installing dependencies..."
-apt-get update && apt-get install -y unzip wget curl  # Ensure package list is updated
-
-echo "Installing Terraform..."
-TERRAFORM_VERSION="1.10.5"  # Specify the version you want to install
-TERRAFORM_ZIP="terraform_${TERRAFORM_VERSION}_linux_amd64.zip"
-wget https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/${TERRAFORM_ZIP}
-unzip ${TERRAFORM_ZIP}
-mv terraform /usr/local/bin/
-rm ${TERRAFORM_ZIP}
-
-
-echo "dependencies n Terraform installation complete."
+mv /root/WE3-C02-IaC/Root/*   /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/
--- Root/scripts/05_application_start.sh
+++ Root/scripts/05_application_start.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+echo "Initializing Terraform..."
+cd /opt/terraform-project/WE3-C02-IaC/Root
+terraform init
+
+
+#!/bin/bash
 
 echo "Applying Terraform configurations..."
 cd /opt/terraform-project/WE3-C02-IaC/Root
