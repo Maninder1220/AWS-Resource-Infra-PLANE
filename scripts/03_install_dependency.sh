ee0b3ad79319405c8ac5940e5f2bc2481b4ae739
Maninder1220 <maninderjitsinghjaura@gmail.com>
2025-01-29 00:53:06 +0530

    Cnge

--- scripts/03_install_dependency.sh
+++ scripts/03_install_dependency.sh
@@ -7,7 +7,7 @@ apt update -y
 apt install -y unzip curl
 
 # Install Terraform
-TERRAFORM_VERSION="1.8.3"  # Adjust as needed
+TERRAFORM_VERSION="1.10.5"  # Adjust as needed
 curl -O https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
 unzip terraform_${TERRAFORM_VERSION}_linux_amd64.zip -d /usr/local/bin/
 rm terraform_${TERRAFORM_VERSION}_linux_amd64.zip
